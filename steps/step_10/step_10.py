70478ddf28617a6899847ac1ddb048a9efcb6d3d
Harald Rietdijk <h.h.rietdijk@pl.hanze.nl>
2025-10-07 18:22:18 +0200

    Added average graph

--- steps/step_10/step_10.py
+++ steps/step_10/step_10.py
@@ -1,7 +1,7 @@
 from steps.step_generic_code.dataframe_knee_operations import get_dataframe
 from steps.step_generic_code.general_functions import start_logging, get_run_id, complete_run
 from steps.step_10.step_10_filter_methods import get_filter_methods_scores
-from steps.step_10.step_10_plot import plot_results_per_method
+from steps.step_10.step_10_plot import plot_average_per_method, plot_results_per_method
 from steps.step_10.step_10_save_results import save_method_results
 
 
@@ -21,4 +21,5 @@ def do_step_10(app):
     filter_methods_scores = get_filter_methods_scores(dataframes, features)
     plot_results_per_method(folder, filter_methods_scores)
     save_method_results(app, filter_methods_scores, run_id)
+    plot_average_per_method(app, folder, run_id)
     complete_run(app, run_id)
--- steps/step_10/step_10_plot.py
+++ steps/step_10/step_10_plot.py
@@ -1,29 +1,39 @@
+from sqlalchemy.sql import func
+
 import matplotlib.pyplot as plt
 import numpy as np
 
+from database.models.feature_selection_data import SELECTION_METHOD, METHOD_RESULTS, METHOD_RESULTS_FEATURES
 from steps.step_generic_code.general_functions import check_folder
 
-def plot_scoring(scoring, model, method, folder):
+def plot_scoring(scores, method, folder, model = None):
     plt.rcParams["font.family"] = "serif"
-    x = np.arange(1,len(scoring['accuracy'])+1)
+    x = np.arange(1,len(list(scores.values())[0][0])+1)
     fig, ax1 = plt.subplots()
-    ax1.set_xticks(np.arange(1, len(scoring['accuracy'])+1, step = 4))
+    ax1.set_xticks(np.arange(1, len(scores['accuracy'])+1, step = 4))
     ax1.set_xlabel("Nr. of Features used")
-    ax1.set_ylabel('F1-Score and Accuracy', fontsize='large', labelpad=30)
-    lines = ax1.plot(x, scoring['accuracy'],color='tab:red', label='Accuracy')
-    lines = lines + ax1.plot(x, scoring['f1-score'], color='tab:blue', label='F1-score')
+    ax1.set_ylabel('Mertic score', fontsize='large', labelpad=30)
+    lines = None
+    for label, metric in scores.items():
+        if not lines:
+            lines = ax1.plot(x, metric[0], color=metric[1], label=label)
+        else:
+            lines = lines + ax1.plot(x, metric[0], color=metric[1], label=label)
     ax1.legend(lines, [line.get_label() for line in lines], loc='lower right')
-    title = 'Accuracy and F1-score for ' + model + ' using ' + method
+    title = 'Metrics using ' + method
+    name = folder + '/' + method + '.png'
+    if model:
+        title = title + ' for ' + model
+        name = folder + '/' + model + '.png'
     plt.title(title)
-    name = folder + '/' + model + '.png'
     plt.savefig(name, format='png')
 
 def plot_score_per_model_per_nr_features_selected(folder, scores, method):
     for model, model_scores in scores.items():
         scoring = {}
-        scoring['accuracy']  = model_scores['accuracy']
-        scoring['f1-score']  = model_scores['f1-score']
-        plot_scoring(scoring, model, method, folder)
+        scoring['accuracy']  = (model_scores['accuracy'], 'tab:red')
+        scoring['f1-score']  = (model_scores['f1-score'], 'tab:blue')
+        plot_scoring(scoring, method, folder, model)
 
 def plot_results_per_method(folder, scores):
     print('Step 10: plotting filter methods results')
@@ -34,3 +44,28 @@ def plot_results_per_method(folder, scores):
         check_folder(method_folder)
         plot_score_per_model_per_nr_features_selected(method_folder, scores_per_method, method)
 
+def get_scores_per_method(app, run_id):
+    scores_per_method = {}
+    average_accuracy = app.session.query(SELECTION_METHOD.name, METHOD_RESULTS.nr_features, 
+                                         func.avg(METHOD_RESULTS.accuracy), func.avg(METHOD_RESULTS.f1_score),
+                                         func.avg(METHOD_RESULTS.precision), func.avg(METHOD_RESULTS.recall))\
+                                  .join(METHOD_RESULTS, SELECTION_METHOD.id==METHOD_RESULTS.method_id)\
+                                  .filter(METHOD_RESULTS.run_id==run_id)\
+                                  .group_by(SELECTION_METHOD.name, METHOD_RESULTS.nr_features)\
+                                  .order_by(SELECTION_METHOD.name, METHOD_RESULTS.nr_features).all()
+    for row in average_accuracy:
+        if not row[0] in scores_per_method.keys():
+            scores_per_method[row[0]] = {'accuracy' : ([],'tab:red'), 'f1-score' : ([],'tab:blue'), 
+                                         'precision' : ([],'tab:orange'), 'recall' : ([],'tab:green')}
+        scores_per_method[row[0]]['accuracy'][0].append(row[2])
+        scores_per_method[row[0]]['f1-score'][0].append(row[3])
+        scores_per_method[row[0]]['precision'][0].append(row[4])
+        scores_per_method[row[0]]['recall'][0].append(row[5])
+    return scores_per_method
+
+def plot_average_per_method(app, folder, run_id):
+    folder = folder +'/plots/average'
+    check_folder(folder)
+    scores_per_method = get_scores_per_method(app, run_id)
+    for method, method_scores in scores_per_method.items():
+        plot_scoring(method_scores, method, folder)
